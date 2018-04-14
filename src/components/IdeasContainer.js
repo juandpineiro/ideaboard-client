bd850312c2ab8bbd2845752fd3a04fd5063a11a8
Juan Piñeiro <juandpineiro@gmail.com>
2018-04-14 00:47:11 -0400

    Get IdeasContainer component to show all ideas in DB

--- src/components/IdeasContainer.js
+++ src/components/IdeasContainer.js
@@ -21,7 +21,14 @@ class IdeasContainer extends Component {
   render() {
     return (
       <div className="">
-        Ideas
+        {this.state.ideas.map((idea) => {
+          return (
+            <div className="tile" key={idea.id}>
+              <h4>{idea.title}</h4>
+              <p>{idea.body}</p>
+            </div>
+          )
+        })}
       </div>
     );
   }
