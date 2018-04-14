dba302a30e44337ff2cfe4901ad8d84795450545
Juan Piñeiro <juandpineiro@gmail.com>
2018-04-14 15:10:30 -0400

    Add form to edit ideas (IdeaForm component)

---
+++ src/components/IdeaForm.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+
+class IdeaForm extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      title: this.props.idea.title,
+      body: this.props.idea.body
+    }
+  }
+
+  handleInput = (e) => {this.setState({[e.target.name]: e.target.value})}
+
+  render () {
+    return (
+      <div className="tile">
+        <form>
+          <input className="input" type="text" name="title" 
+                 placeholder="Enter a title" value={this.state.title} 
+                 onChange={this.handleInput} />
+          <textarea className="input" name="body" 
+                    placeholder="Describe your idea" value={this.state.body} 
+                    onChange={this.handleInput} />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default IdeaForm
