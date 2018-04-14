24f902e674d07d720295842ca965ea5be2d08177
Juan Piñeiro <juandpineiro@gmail.com>
2018-04-14 17:18:37 -0400

    Finish idea editing functionality

--- src/components/IdeasContainer.js
+++ src/components/IdeasContainer.js
@@ -40,6 +40,10 @@ class IdeasContainer extends Component {
 
   resetNotification = () => {this.setState({notification: ''})}
 
+  enableEditing = (id) => {
+    this.setState({editingIdeaId: id}, () => { this.title.focus() })
+  }
+  
   render() {
     return (
       <div>
@@ -54,10 +58,12 @@ class IdeasContainer extends Component {
         {this.state.ideas.map((idea) => {
           if (this.state.editingIdeaId === idea.id) {
             return ( <IdeaForm idea={idea} key={idea.id} 
-                               updateIdea={this.updateIdea} 
+                               updateIdea={this.updateIdea}
+                               titleRef={input => this.title = input}
                                resetNotification={this.resetNotification} /> )
           } else {
-            return ( <Idea idea={idea} key={idea.id} /> )
+            return ( <Idea idea={idea} key={idea.id} 
+                           onClick={this.enableEditing} /> )
           }
         })}
       </div>
