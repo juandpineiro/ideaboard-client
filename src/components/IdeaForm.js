bc322a38eb7e2f9f543bc7ac3b9612df173538f3
Juan Piñeiro <juandpineiro@gmail.com>
2018-04-14 16:58:23 -0400

    Add new idea editing and updating functionality with notification

--- src/components/IdeaForm.js
+++ src/components/IdeaForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 class IdeaForm extends Component {
   constructor(props) {
@@ -9,12 +10,26 @@ class IdeaForm extends Component {
     }
   }
 
-  handleInput = (e) => {this.setState({[e.target.name]: e.target.value})}
+  handleInput = (e) => {
+    this.props.resetNotification()
+    this.setState({[e.target.name]: e.target.value})
+  }
+
+  handleBlur = () => {
+    const idea = {title: this.state.title, body: this.state.body}
+    axios.put(`http://localhost:3001/api/v1/ideas/${this.props.idea.id}`,
+               {idea: idea}
+              ).then(response => {
+                console.log(response)
+                this.props.updateIdea(response.data)
+              })
+              .catch(error => console.log(error))
+  }
 
   render () {
     return (
       <div className="tile">
-        <form>
+        <form onBlur={this.handleBlur}>
           <input className="input" type="text" name="title" 
                  placeholder="Enter a title" value={this.state.title} 
                  onChange={this.handleInput} />
